e20623d5c36319233e9890e43eb2f9de37c2487e
John Carr <john.carr@unrouted.co.uk>
2017-12-17 11:12:48 +0000

    Dump entire bitbuffer

--- src/devices/honeywell_activlink.c
+++ src/devices/honeywell_activlink.c
@@ -7,18 +7,16 @@
 #include "util.h"
 
 static int honeywell_callback(bitbuffer_t *bitbuffer) {
-    uint8_t buffer[80];
-    bitbuffer_extract_bytes(bitbuffer, 0, 0, buffer, 128);
+    fprintf(stderr, "bitbuffer:: Number of rows: %d \n", bitbuffer->num_rows);
+    for (uint16_t row = 0; row < bitbuffer->num_rows; ++row) {
+        fprintf(stderr, "[%02d] {%d} ", row, bitbuffer->bits_per_row[row]);
+        for (uint16_t col = 0; col < (bitbuffer->bits_per_row[row]+7)/8; ++col) {
+            fprintf(stderr, "%02x ", bitbuffer->bb[row][col]);
+        }
+        fprintf(stderr, "\n");
+    }
 
-    char raw_str[160];
-    for (unsigned n=0; n<sizeof(buffer); n++) { sprintf(raw_str+n*2, "%02x", buffer[n]); }
-
-    data_t *data = data_make(
-          "payload", "", DATA_STRING, raw_str,
-          NULL);
-
-    data_acquired_handler(data);
-    return 1;
+    return 0;
 }
 
 static char *output_fields[] = {
@@ -28,12 +26,13 @@ static char *output_fields[] = {
 
 r_device honeywell_activlink = {
     .name           = "Honeywell Activlink Doorbell",
-    .modulation     = FSK_PULSE_PCM,
-    .short_limit    = 39,
-    .long_limit     = 39,
-    .reset_limit    = 39936,
+    .modulation     = OOK_PULSE_PWM_TERNARY,
+    .short_limit    = 64,
+    .long_limit     = 128,
+    .reset_limit    = 1200,
     .json_callback  = &honeywell_callback,
     .disabled       = 0,
-    .demod_arg      = 0,
+    .demod_arg      = 2,
     .fields         = output_fields,
 };
+
